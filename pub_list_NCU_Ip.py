706797fcbd623bfa12e6dc89f7c28006861eb917
anlitsai <altsai.tw@gmail.com>
2020-05-16 03:30:06 +0800

    update_20200516-0330

---
+++ pub_list_NCU_Ip.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 16 02:33:49 2020
+
+@author: altsai
+"""
+
+
+import os
+import sys
+import shutil
+#import re
+import subprocess
+#import numpy as np
+import pandas as pd
+file_IP='NCU_Ip.txt'
+
